5c83db555b6b6202c4dd7440bc2931f1647531e3
javie <javie@MSI>
2023-06-04 13:02:42 -0600

    Inicio

---
+++ src/main/java/com/mycompany/grupo_3_pfa_avance/Grupo_3_PFA_Avance.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupo_3_pfa_avance;
+
+/**
+ *
+ * @author javie
+ */
+public class Grupo_3_PFA_Avance {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
