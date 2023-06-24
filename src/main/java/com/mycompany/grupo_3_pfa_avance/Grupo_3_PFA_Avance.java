5e0fc8b93558c3d64fc9eb294530a9c9c357fb5a
javie <javie@MSI>
2023-06-24 16:37:09 -0600

    Avance...

--- src/main/java/com/mycompany/grupo_3_pfa_avance/Grupo_3_PFA_Avance.java
+++ src/main/java/com/mycompany/grupo_3_pfa_avance/Grupo_3_PFA_Avance.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.grupo_3_pfa_avance;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author javie
@@ -11,6 +13,35 @@ package com.mycompany.grupo_3_pfa_avance;
 public class Grupo_3_PFA_Avance {
 
     public static void main(String[] args) {
-        System.out.println("Holaaaa!");
+                //...............
+        int opcion = 0;
+        //...............
+        while(opcion != 7) {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                                        "Menú Inicial:\n"
+                                        + "1 - Registrarse\n"
+                                        + "2 - Iniciar Seccion\n"
+                                        + "3 - Horarios\n"
+                                        + "4 - Salir\n"
+                                        + "Digite una opción para continuar: "
+            ));
+            switch(opcion) {
+                case 1:
+                    System.out.println("-------");
+                    break;
+                case 2:
+                    System.out.println("-------");
+                    break;
+                case 3:
+                    System.out.println("-------");
+                    break;
+                case 4:
+                    System.out.println("-------");
+                    break;
+                default:
+                    System.out.println("Opción Invalida...");
+                    break;
+            }
+        }
     }
 }
